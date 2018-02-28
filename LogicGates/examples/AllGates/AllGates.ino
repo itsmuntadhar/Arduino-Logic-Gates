5c83e136f047a85e1993e01f41d993c033b0b8cf
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2018-02-28 16:31:01 +0100

    Add files via upload

---
+++ LogicGates/examples/AllGates/AllGates.ino
@@ -0,0 +1,30 @@
+bool a;
+bool b;
+bool c;
+bool d;
+bool e;
+bool f;
+bool g;
+
+#include <LogicGates.h>
+
+void setup() {
+  Serial.begin(9600);
+  a=NotGate(true);
+  Serial.println(a);
+  b=AndGate(true,true);
+  Serial.println(b);
+  c=NandGate(true,true);
+  Serial.println(c);
+  d=OrGate(true,true);
+  Serial.println(d);
+  e=NorGate(true,true);
+  Serial.println(e);
+  f=XorGate(true,true);
+  Serial.println(f);
+  g=XnorGate(true,true);
+  Serial.println(g);
+}
+
+void loop() {
+}
